09f720ca6e5bb03d79c391259b627fb78abaad91
Adam Buckley <Adam Buckley>
2016-11-11 09:46:48 +0000

    Hacker News - Add post feature completed

--- hackerNews/src/App.js
+++ hackerNews/src/App.js
@@ -1,86 +1,144 @@
- import React from 'react';
+    import React from 'react';
+    import _ from 'lodash';
+    import api from './test/stubAPI';
 
-var Form = React.createClass({
-  render : function() {
-	return (
-	   <form style={{marginTop: '30px'}}>
-		  <h3>Add a new post</h3>
+    var Form = React.createClass({
+       getInitialState: function() {
+           return { title: '', link: ''};
+        },
+		
+       handleTitleChange: function(e) {
+           this.setState({title: e.target.value});
+       },
+	   
+       handleLinkChange: function(e) {
+           this.setState({link: e.target.value});
+       },
+	   
+	    handleSubmit: function(e) { {/* submit is the add button! */}
+        e.preventDefault();
+        var title = this.state.title.trim();
+        var link = this.state.link.trim();
+        if (!title ) {
+          return;
+        }
+        this.props.addHandler(title,link);
+        this.setState({title: '', link: ''});
+       }, 
+		
+        render : function() {
+			
+           return (
+		   
+             <form style={{marginTop: '30px'}}>
+                <h3>Add a new post</h3>
+                <div className="form-group">
+                  <input type="text"
+                    className="form-control" placeholder="Title"
+                    value={this.state.title} 
+					onChange={this.handleTitleChange} ></input>
+                </div>
+                <div className="form-group">
+                  <input type="text"
+                     className="form-control" placeholder="Link"
+                     value={this.state.link} 
+					 onChange={this.handleLinkChange} ></input>
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Post</button>
+              </form>
+            );
+          }
+       });
 
-		  <div className="form-group">
-			<input type="text"
-			  className="form-control"
-			  placeholder="Title"></input>
-		  </div>
-		  <div className="form-group">
-			<input type="text"
-			className="form-control"
-			placeholder="Link"></input>
-		  </div>
-		  <button type="submit" className="btn btn-primary">Post</button>
-		</form>
-		);
-	}
- });
-
-var NewsItem = React.createClass({
-  render : function() {
-	  var divStyle = {
-		   fontSize: '20px', 
-		   marginLeft: '10px' 
-		  };
-	  var cursor = { cursor: 'pointer' } ;
-	  var line ;
-	  if (this.props.post.link ) {
-		 line = <a href={this.props.post.link} >
-		{            this.props.post.title} </a> ;
-	  } else {
-		 line = <span>{this.props.post.title} </span> ;
-	  }
-	return (
-		<div >
-		  <span className="glyphicon glyphicon-thumbs-up"
-				style={cursor} />
-		  {this.props.post.upvotes}
-		  <span style={divStyle}>{line}
-			  <span>
-			  <a href={'#/posts/' + this.props.post.id }>Comments</a>
-			</span>
-		  </span>
-		</div>  
-		);
-  }
- }) ;
-
-var NewsList = React.createClass({
-          render: function(){
-			   {/* the postRows variable is assigned all the posts with each post's link being used as their unique key (unique identifier). */}
-              var postsRows = this.props.posts.map( newsItem =>
-			  { return <NewsItem key={newsItem.link} post={newsItem} /> });
-              return (
-                  <p>
-				  {/* display all the posts. */}
-                      {postsRows}
-                      <Form />
-                  </p>
-                ) ;
+    var NewsItem = React.createClass({
+		
+			getInitialState : function() {
+               return {
+                status : '',
+                title: this.props.post.title,
+                link: this.props.post.address,
+               } ;
+            },
+	
+        handleVote : function() {
+          this.props.upvoteHandler(this.props.post.id);
+        },
+		
+        render : function() {
+			
+            var lineStyle = {
+                 fontSize: '20px', marginLeft: '10px'  };
+            var cursor = { cursor: 'pointer' } ;
+            var line ;
+            if (this.props.post.link ) {
+               line = <a href= {this.props.post.link} > {/* q to ask: why can't i do a href = "http://www." + {this........link} */}
+                            {this.props.post.title} </a> ;
+            } else {
+               line = <span>{this.props.post.title} </span> ;
             }
-});
+            return (
+              <div >
+                <span className="glyphicon glyphicon-thumbs-up" 
+                    style={cursor} 
+                    onClick={this.handleVote} ></span>
+                {this.props.post.upvotes}
+                <span style={lineStyle} >{line}<span>
+                    <a href={'#/posts/' + this.props.post.id }>Comments</a>
+                  </span>
+                </span>
+              </div>  
+        );
+        }
+       }) ;
 
- var HackerApp = React.createClass({
-   render: function(){
-	return (
-		<div className="container">
-		  <div className="row">
-			<div className="col-md-6 col-md-offset-3">
-		 <div className="page-header">
-                       <h1>Hacker News</h1>
-                       <NewsList posts={this.props.posts}  />
-		 </div>
-		   </div>
-		  </div>
-		</div>
-	);
-  }
-});
+    var NewsList = React.createClass({
+        render : function() {
+          var items = this.props.posts.map(function(post,index) {
+             return <NewsItem key={index} post={post} 
+                        upvoteHandler={this.props.upvoteHandler}
+						addHandler={this.props.addHandler} /> ;
+            }.bind(this) )
+          return (
+            <div>
+                  {items}
+                  </div>
+            );
+        }
+    }) ;  
 
-export default HackerApp;
+    var HackerApp = React.createClass({
+		
+        incrementUpvote : function(id) {
+          api.upvote(id) ;
+          this.setState({});
+        },    
+		  
+		 addPost : function(t,l) {
+            if (api.add(t,l)) {
+             this.setState({});
+			}
+          },
+		  
+        render: function(){
+            var posts = _.sortBy(api.getAll(), function(post) {
+                    return - post.upvotes;
+                 }
+              );
+            return (
+              <div className="container">
+                 <div className="row">
+                    <div className="col-md-6 col-md-offset-3">
+                       <div className="page-header">
+                          <h1>Hacker News</h1>
+                             <NewsList posts={posts} 
+                                  upvoteHandler={this.incrementUpvote}  />
+								  <Form addHandler={this.addPost} />
+                       </div>
+                     </div>
+                  </div>
+                </div>
+            );
+        }
+    });
+
+    export default HackerApp;
